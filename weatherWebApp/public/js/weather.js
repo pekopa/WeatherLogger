2ca43ecc0e066c0eacbcaaa4f6e79e0bb7f36e9c
Pixochi <kozak.jakub55@gmail.com>
2017-12-05 12:33:26 +0100

    comparing graphs

--- weatherWebApp/public/js/weather.js
+++ weatherWebApp/public/js/weather.js
@@ -11,10 +11,25 @@ function drawGraph(graphId, graphData) {
     scaleFontColor: "#c5c7cc"
   }
 
-	var chartElement = document.getElementById(graphId).getContext("2d");
-  window.myLine = new Chart(chartElement).Line(graphData, chartOptions);
-
+  resetCanvas(graphId);
+	var canvasCtx = document.getElementById(graphId).getContext("2d");
+	canvasCtx.clearRect(0, 0, 10000, 100000);
+  window.myLine = new Chart(canvasCtx).Line(graphData, chartOptions);
 }
+
+var resetCanvas = function(canvasId){
+  $('#'+canvasId).remove(); // this is my <canvas> element
+  $('.canvas-wrapper.'+canvasId).append('<canvas class="main-chart" id="'+canvasId+'"><canvas>');
+  canvas = document.querySelector('#'+canvasId);
+  ctx = canvas.getContext('2d');
+  ctx.canvas.width = $('.canvas-wrapper.'+canvasId).width(); // resize to parent width
+  ctx.canvas.height = $('.canvas-wrapper.'+canvasId).height(); // resize to parent height
+  var x = canvas.width/2;
+  var y = canvas.height/2;
+  // ctx.font = '10pt Verdana';
+  // ctx.textAlign = 'center';
+  // ctx.fillText('This text is centered on the canvas', x, y);
+};
 
 function getDateLabels(data, format) {
 
@@ -36,7 +51,6 @@ function getDateLabels(data, format) {
 				case 'TT':
 					var month = dateObj.getMonth();
 					var monthAsWord = MONTHS[month];
-					console.log(MONTHS)
 					return monthAsWord;
 					break;
 				case 'DD':
@@ -97,6 +111,27 @@ function getWeatherPropsData(data) {
   }
 }
 
+function getOpenWeatherProps(data) {
+	var temperature = [],
+			humidity = [],
+			pressure = [],
+			windSpeed = [];
+
+	data.forEach(function(measurement) {
+      temperature.push(measurement.main.temp);
+      humidity.push(measurement.main.humidity);
+      pressure.push(measurement.main.pressure);
+      windSpeed.push(measurement.wind.speed);
+  });
+
+  return {
+  	temperature: temperature,
+  	humidity: humidity,
+  	pressure: pressure,
+  	windSpeed: windSpeed
+  }
+}
+
 function fetchWeatherData(timeStampStart, timeStampEnd) {
 
 	if (isNaN(timeStampStart) || isNaN(timeStampEnd)) {
@@ -106,18 +141,24 @@ function fetchWeatherData(timeStampStart, timeStampEnd) {
 	var apiUrl = 'http://restfullservicefordatalogger.azurewebsites.net/Weather.svc/WeatherMeasurements/';
 	apiUrl += timeStampStart + "/" + timeStampEnd; 
 
-	console.log('tiimestamps')
-	console.log(apiUrl)
-
 	var proxy = 'https://cors-anywhere.herokuapp.com/';
 
-	var graphConfig = {
-    fillColor: "rgba(220,220,220,0.2)",
-    pointColor:"rgba(220,220,220,1)",
+	var sensorGraphConfig = {
+    fillColor: "rgba(220,220,220,0)",
+    pointColor:"rgba(0,0,255,1)",
+    pointHighlightFill:"#fff",
+    pointHighlightStroke:"rgba(0,0,255,1)",
+    pointStrokeColor:"#fff",
+    strokeColor:"rgba(0,0,255,1)"
+  }
+
+  var openWeatherGraphConfig = {
+    fillColor: "rgba(255,0,0,0)",
+    pointColor:"rgba(255,0,0,1)",
     pointHighlightFill:"#fff",
     pointHighlightStroke:"rgba(220,220,220,1)",
     pointStrokeColor:"#fff",
-    strokeColor:"rgba(220,220,220,1)"
+    strokeColor:"rgba(255,0,0,1)"
   }
 
 	$.get(proxy+apiUrl, function(data) {
@@ -130,52 +171,37 @@ function fetchWeatherData(timeStampStart, timeStampEnd) {
 		var avgSensorValues = getAvgValues(data);
 		var dataByProps = getWeatherPropsData(data);
 
-		console.log(dataByProps)
 		// 	TEMPERATURE
-		var datasetSensorTemperature = {
-        data: dataByProps.temperature,
-        graphConfig
-    }
+		var datasetSensorTemperature = sensorGraphConfig;
+		datasetSensorTemperature.data = dataByProps.temperature;
+
 		var SensorTemperatureGraphData = {
        datasets: [datasetSensorTemperature],
        labels: labels
     }
 
     // HUMIDITY
-    var datasetSensorHumidity = {
-        data: dataByProps.humidity,
-        graphConfig
-    }
+    var datasetSensorHumidity = sensorGraphConfig;
+    datasetSensorHumidity.data = dataByProps.humidity;    
+        
 		var SensorHumidityGraphData = {
        datasets: [datasetSensorHumidity],
        labels: labels
     }
 
     // PRESSURE
-    var datasetSensorPressure = {
-        data: dataByProps.pressure,
-        graphConfig
-    }
+    var datasetSensorPressure = sensorGraphConfig;
+    datasetSensorPressure.data = dataByProps.pressure;
+
 		var SensorPressureGraphData = {
        datasets: [datasetSensorPressure],
        labels: labels
     }
 
-    // HUMIDITY
-    var datasetSensorHumidity = {
-        data: dataByProps.humidity,
-        graphConfig
-    }
-		var SensorHumidityGraphData = {
-       datasets: [datasetSensorHumidity],
-       labels: labels
-    }
-
     // WIND SPEED
-    var datasetSensorWindSpeed = {
-        data: dataByProps.windSpeed,
-        graphConfig
-    }
+    var datasetSensorWindSpeed = sensorGraphConfig;
+    datasetSensorWindSpeed.data = dataByProps.windSpeed;
+
 		var SensorWindSpeedGraphData = {
        datasets: [datasetSensorWindSpeed],
        labels: labels
@@ -184,15 +210,26 @@ function fetchWeatherData(timeStampStart, timeStampEnd) {
     var url = "../../controllers/_3rdPartyWeather.php?date-start="+timeStampStart+"&date-end="+timeStampEnd;
 
     $.get(url, function(data) {
-    	console.log('OPEN WEATHER')
+    	
     	var apiWeather = JSON.parse(data);
-    	console.log(apiWeather)
-    });
+    	var openWeather = getOpenWeatherProps(apiWeather.list);
 
-    drawGraph('temperature-chart', SensorTemperatureGraphData);
-    drawGraph('humidity-chart', SensorHumidityGraphData);
-    drawGraph('pressure-chart', SensorPressureGraphData);
-		drawGraph('windSpeed-chart', SensorWindSpeedGraphData);
+    	var openWeatherDatasets = {};
+    	Object.keys(openWeather).forEach(function(key) {
+    		openWeatherDatasets[key] = Object.assign({}, openWeatherGraphConfig);
+    		openWeatherDatasets[key].data = openWeather[key];
+    	});
+
+    	SensorTemperatureGraphData.datasets.push(openWeatherDatasets.temperature);
+    	SensorHumidityGraphData.datasets.push(openWeatherDatasets.humidity);
+    	SensorPressureGraphData.datasets.push(openWeatherDatasets.pressure);
+    	SensorWindSpeedGraphData.datasets.push(openWeatherDatasets.windSpeed);
+
+    	drawGraph('temperature-chart', SensorTemperatureGraphData);
+	    drawGraph('humidity-chart', SensorHumidityGraphData);
+	    drawGraph('pressure-chart', SensorPressureGraphData);
+			drawGraph('windSpeed-chart', SensorWindSpeedGraphData);
+    });
 	});
 }
 
